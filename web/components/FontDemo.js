03361029429a3ee8460943872112f18234b4b8fc
border-radius <pharshmaster@gmail.com>
2019-03-23 21:03:06 +0300

    Editable

--- web/components/FontDemo.js
+++ web/components/FontDemo.js
@@ -1,9 +1,37 @@
 import styled from 'styled-components'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
-const defaultText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto quo labore nulla. Corporis possimus asperiores sit voluptates debitis voluptas, pariatur accusamus. Cupiditate ipsum tempora, natus earum ex hic nesciunt soluta!';
+const defaultText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+Architecto quo labore nulla.
+Corporis possimus asperiores sit voluptates debitis voluptas, pariatur accusamus.
+Cupiditate ipsum tempora, natus earum ex hic nesciunt soluta!`.replace(/\n/g, ' ')
+
+const TextArea = ({ value, onChange, className }) => (
+    <textarea
+        value={ value }
+        onChange={ onChange }
+        className={ className }
+        autocomplete='off'
+        autocorrect='off'
+        autocapitalize='off'
+        spellcheck='false'
+    />
+)
+
+const Editor = styled(TextArea)`
+    margin: 0 22px;
+    width: ${ props => props.width }px;
+    height: 200px;
+    font-family: '${ props => props.family }';
+    font-weight: ${ props => props.font };
+    font-size: ${ props => props.size }px;
+    border: none;
+`
 
 const FontDemo = ({ context }) => {
+    const [ text, setText ] = useState(defaultText)
+    const onChange = ({ target }) => setText(target.value)
+
     const {
         family,
         font,
@@ -11,15 +39,16 @@ const FontDemo = ({ context }) => {
         width,
     } = useContext(context)
 
-    const Paragraph = styled.p`
-        padding: 0 22px;
-        width: ${width}px;
-        font-family: '${ family }';
-        font-weight: ${ font };
-        font-size: ${ size }px;
-    `
-
-    return <Paragraph>{ defaultText }</Paragraph>
+    return (
+        <Editor
+            width={ width }
+            family={ family }
+            font={ font }
+            size={ size }
+            onChange={ onChange }
+            value={ text }
+        />
+    )
 }
 
 export default FontDemo
